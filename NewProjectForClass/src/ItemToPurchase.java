d40118c4ec1461a7b6eb991fb43ba9e72e88415f
pjsena <pjsena@10.0.0.6>
2020-10-15 12:43:06 -0400

    Added all the instance variable

--- NewProjectForClass/src/ItemToPurchase.java
+++ NewProjectForClass/src/ItemToPurchase.java
@@ -1,4 +1,20 @@
 
 public class ItemToPurchase {
-
+/*
+ * Private fields
+ * String itemName - Initialized in default constructor to "none"
+ * int itemPrice - Initialized in default constructor to 0
+ * int itemQuantity - Initialized in default constructor to 0
+ * 
+ * Default constructor - one that does not take any parameters
+ * 	eg. ItemToPurchase() -> is a default constructor
+ */
+	
+	String itemName;
+	int itemPrice;
+	int itemQuantity;
+	
+	ItemToPurchase(String itemName) {
+		
+	}
 }
--- NewProjectForClass/src/ShoppingCartPrinter.java
+++ NewProjectForClass/src/ShoppingCartPrinter.java
@@ -1,6 +1,6 @@
 
-public class WhatToDo {
+public class ShoppingCartPrinter {
 	
-	
+	String name;
 
 }
