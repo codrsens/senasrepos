402f58c3b2ec28b6f203572d037da4f4bad2c632
pjsena <pjsena@10.0.0.6>
2020-10-15 21:45:03 -0400

    assigning the homework

--- NewProjectForClass/src/ItemToPurchase.java
+++ NewProjectForClass/src/ItemToPurchase.java
@@ -14,6 +14,32 @@ public class ItemToPurchase {
 	int itemPrice;
 	int itemQuantity;
 	
+	public String getItemName() {
+		return itemName;
+	}
+
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+
+	public int getItemPrice() {
+		return itemPrice;
+	}
+
+	public void setItemPrice(int itemPrice) {
+		this.itemPrice = itemPrice;
+	}
+
+	public int getItemQuantity() {
+		return itemQuantity;
+	}
+
+	public void setItemQuantity(int itemQuantity) {
+		this.itemQuantity = itemQuantity;
+	}
+
+	
+	
 	ItemToPurchase(String itemName) {
 		
 	}
